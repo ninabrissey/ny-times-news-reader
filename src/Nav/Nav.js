cb5b4d02bb7de2e18d8a88affd06b9c0813ebba7
Nina Brissey <nbrissey@gmail.com>
2021-11-01 19:34:20 -0700

    Add nav content

---
+++ src/Nav/Nav.js
@@ -0,0 +1,29 @@
+// import { NavLink } from 'react-router-dom';
+import '../Nav/Nav.css';
+
+const Nav = () => {
+  return (
+    <nav className="nav-bar">
+      <p>to be img icon</p>
+      <ul className="nav-links">
+        {/* <NavLink> */}
+        <li>Home</li>
+        {/* </NavLink>
+        <NavLink> */}
+        <li>Arts</li>
+        {/* </NavLink>
+        <NavLink> */}
+        <li>Science</li>
+        {/* </NavLink>
+        <NavLink> */}
+        <li>US</li>
+        {/* </NavLink>
+        <NavLink> */}
+        <li>World</li>
+        {/* </NavLink> */}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
