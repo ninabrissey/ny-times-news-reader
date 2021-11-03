f8ce1ef0ce03fa84801c690f85efd4b0988efa41
Nina Brissey <nbrissey@gmail.com>
2021-11-03 17:04:12 -0600

    Build Container logic

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,1 +1,46 @@
 // if this is not reusable then the filename needs to change
+import { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+import { getData } from '../utils/apiCalls';
+import '../Container/Container.css';
+
+const Container = ({ section }) => {
+  const [currentSection, setCurrentSection] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState('');
+
+  useEffect(() => {
+    const getSection = async () => {
+      try {
+        const data = await getData(section);
+        setCurrentSection(data.results);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setErr(error.message);
+      }
+    };
+    getSection();
+  }, [section]);
+
+  //need to add a actual key on the Card component using something like uuid
+
+  return (
+    <div className="container">
+      {isLoading ? (
+        <p>loading</p>
+      ) : (
+        <section>
+          {currentSection.map((article) => {
+            console.log(article);
+            return (
+              <Card key={article.title} section={section} article={article} />
+            );
+          })}
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default Container;
