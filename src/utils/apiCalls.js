b3dc0c7cd650be98e2774e9bc5da8630dff33562
Nina Brissey <nbrissey@gmail.com>
2021-11-03 17:07:44 -0600

    Adjust api call - basic barebones

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,25 +1,11 @@
-// const API_KEY = `${process.env.REACT_APP_NY_TIMES_KEY}=${process.env.REACT_APP_NY_TIMES_SECRET_KEY}`;
-
-// https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=yourkey
-// https://api.nytimes.com/svc/topstories/v2/home.json?api-key=yourkey
-// https://api.nytimes.com/svc/topstories/v2/science.json?api-key=yourkey
-// https://api.nytimes.com/svc/topstories/v2/us.json?api-key=yourkey
-// https://api.nytimes.com/svc/topstories/v2/world.json?api-key=yourkey
-
-export const getData = async (section) => {
-  try {
-    const data = await fetch(
-      'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=VUy19Fl88GH7UARSGbrbazb9U2MqOVdj',
-      {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-      }
-    );
-    const articles = await data.json();
-    console.log(articles);
-  } catch (error) {
-    console.log(error);
-  }
+export const getData = (section) => {
+  return fetch(
+    `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=VUy19Fl88GH7UARSGbrbazb9U2MqOVdj`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+  ).then((res) => res.json());
 };
